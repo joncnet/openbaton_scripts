7806b0ddb1b35cbcd1f077f27dc1635513746012
root <g.baggio@create-net.org>
2016-05-16 08:33:22 +0000

    fix

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 echo "second test" > /home/ubuntu/output.txt
 echo "printing private ip" >> /home/ubuntu/output.txt
-echo $instance_private >> /home/ubuntu/output.txt
+echo $instance_vlab >> /home/ubuntu/output.txt
 printenv >> /home/ubuntu/output.txt
