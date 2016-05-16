17e95a887ba2abcff1349bfd8c7eb8a3cf78f663
root <g.baggio@create-net.org>
2016-05-16 08:00:40 +0000

    edits in test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
-echo "printing private ip" > /home/ubuntu/output.txt
+echo "second test" > /home/ubuntu/output.txt
+echo "printing private ip" >> /home/ubuntu/output.txt
 echo $instance_private >> /home/ubuntu/output.txt
+echo printenv >> /home/ubuntu/output.txt
