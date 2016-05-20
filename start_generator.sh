c0d7d66f55e47a63c833193af49fc0e5eb9b186e
g.baggio@create-net.org <g.baggio@create-net.org>
2016-05-20 08:36:34 +0000

    add user

--- start_generator.sh
+++ start_generator.sh
@@ -1,1 +1,2 @@
-screen -d -m -S generator cvlc /home/ubuntu/BigBuckBunny_320x180.mp4 --sout '#standard{access=http,mux=ts}'
+adduser --disabled-password --gecos "" vlc_user
+su vlc_user -c 'screen -d -m -S generator cvlc /home/ubuntu/BigBuckBunny_320x180.mp4 --sout '#standard{access=http,mux=ts}''
