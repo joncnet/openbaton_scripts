1017ff0dfd07d0086eb777fc9bc12c47948536a4
g.baggio@create-net.org <g.baggio@create-net.org>
2016-05-20 08:41:00 +0000

    fix

--- generator_start_transcoder.sh
+++ generator_start_transcoder.sh
@@ -1,2 +1,2 @@
 adduser --disabled-password --gecos "" vlc_user
-su vlc_user -c creen -d -m -S transcoder cvlc -v http://$generator_bridge:8080 :sout='#transcode{vcodec=h264,vfilter=sepia,scale=Auto}:http{mux=ts,dst=:8080/}' :sout-keep
+su vlc_user -c "screen -d -m -S transcoder cvlc -v http://$generator_bridge:8080 :sout='#transcode{vcodec=h264,vfilter=sepia,scale=Auto}:http{mux=ts,dst=:8080/}' :sout-keep"
