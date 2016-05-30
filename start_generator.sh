969e1b9ba47531066feebc93e70a07cc16f09cbe
g.baggio@create-net.org <g.baggio@create-net.org>
2016-05-30 10:45:22 +0000

    add loop

--- start_generator.sh
+++ start_generator.sh
@@ -1,2 +1,2 @@
 adduser --disabled-password --gecos "" vlc_user
-su vlc_user -c "screen -d -m -S generator cvlc /home/ubuntu/BigBuckBunny_320x180.mp4 --sout '#standard{access=http,mux=ts}'"
+su vlc_user -c "screen -d -m -S generator cvlc /home/ubuntu/BigBuckBunny_320x180.mp4 --sout '#standard{access=http,mux=ts}' :input-repeat=-1"
