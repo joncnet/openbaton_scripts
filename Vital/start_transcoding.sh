2b9db9fc511e7ac24fa274afd7213539364a6829
g-baggio <g.baggio@create-net.org>
2017-02-27 15:28:45 +0100

    fix

--- Vital/start_transcoding.sh
+++ Vital/start_transcoding.sh
@@ -1,3 +1,3 @@
 adduser --disabled-password --gecos "" vlc_user
 sleep 10
-su vlc_user -c "screen -d -m -S transcoder cvlc -v http://$source_ip:$source_port :sout='#transcode{vcodec=$codec,vfilter=$filter,scale=Auto}:http{mux=$mux,dst=:$dest_port/}' :sout-keep"
+su vlc_user -c "screen -d -m -S transcoder cvlc -v $source_access://$source_ip:$source_port :sout='#transcode{vcodec=$codec,vfilter=$filter,scale=Auto}:$dest_access{mux=$mux,dst=$dest_address:$dest_port/}' :sout-keep"
